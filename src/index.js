1d28c7392585e3739a8271fabd8668932417d4fa
Muhammad Mustafa <75218580+mmustafa93@users.noreply.github.com>
2025-01-22 22:28:36 +0500

    Add Menu to webpage and link it to the menu buttons in the header and footer

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 import "./styles.css";
 import Home from "./home.js";
+import Menu from "./menu.js";
 import locationAndHours from "./locationsAndHours.js";
 
 const homeBtn = document.querySelector("#home-btn");
+const menuBtn = document.querySelectorAll(".menu-btn");
 const locationsAndHoursBtn = document.querySelector("#locations-hours-btn");
 const contentContainer = document.querySelector("#content");
 
@@ -12,6 +14,11 @@ homeBtn.addEventListener("click", () => {
     contentContainer.textContent = "";
     contentContainer.innerHTML = Home();
 });
+
+menuBtn.forEach((btn) => btn.addEventListener("click", () => {
+    contentContainer.textContent = "";
+    contentContainer.innerHTML = Menu();
+}));
 
 locationsAndHoursBtn.addEventListener("click", () => {
     contentContainer.textContent = "";
---
+++ src/menu.js
@@ -0,0 +1,19 @@
+export default function Menu() {
+    return `
+        <div id="menu">
+            <div id="menu-text">
+                <h1>Menu</h1>
+                <p>Our menu is a collaboration between Professional Skateboarder Neen Williams and Michelin Starred Chef Phillip Frankland Lee of Sushi by Scratch Restaurants & Pasta Bar.</p>
+                <div id="burger">
+                    <h3>NADC Burger</h3>
+                    <p>Double patty of 100% full-blooded RC Ranch wagyu beef, American cheese, secret sauce, onions, pickles, slightly tamed jalapeños</p>
+                </div>
+                <div id="sides>
+                    <h3>Sides</h3>
+                    <p>Fries: Hand-cut fries, sweet potato fries, onion rings</p>
+                    <p>Beast Mode Fries: fries with added cheese, diced pickles & slightly tamed jalapeños, special sauce & seasoning</p>
+                </div>
+            </div>
+        </div>
+    `
+}
