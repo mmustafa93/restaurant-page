7f2c19cc972862576fa0f61e3a337d04e4874e05
Muhammad Mustafa <75218580+mmustafa93@users.noreply.github.com>
2025-01-22 21:49:09 +0500

    Add Locations and Hours page

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 import "./styles.css";
 import Home from "./home.js";
+import locationAndHours from "./locationsAndHours.js";
 
 const homeBtn = document.querySelector("#home-btn");
+const locationsAndHoursBtn = document.querySelector("#locations-hours-btn");
 const contentContainer = document.querySelector("#content");
 
 contentContainer.innerHTML = Home();
@@ -9,4 +11,9 @@ contentContainer.innerHTML = Home();
 homeBtn.addEventListener("click", () => {
     contentContainer.textContent = "";
     contentContainer.innerHTML = Home();
+});
+
+locationsAndHoursBtn.addEventListener("click", () => {
+    contentContainer.textContent = "";
+    contentContainer.innerHTML = locationAndHours();
 });
---
+++ src/locationsAndHours.js
@@ -0,0 +1,118 @@
+export default function locationAndHours() {
+    return `
+    <div id="locations-hours">
+    <h1>Locations & Hours</h1>
+    
+    <div id="locations">
+        <div id="chicago">
+        <h2>Chicago</h2>
+        <div id="chicago-p">
+        <p>All Ages welcome!</p>
+        <p>415 N Milwaukee Avenue<br>Chicago, IL</p>
+        <p>
+            Monday: 11:30am-10pm<br>
+            Tuesday: 11:30am-10pm<br>
+            Wednesday: 11:30am-12am<br>
+            Thursday: 11:30am-12am<br>
+            Friday: 11:30am-12am<br>
+            Saturday: 11:30am-12am<br>
+            Sunday: 11:30am-10pm
+        </p>
+        </div>
+        </div>
+    
+        <div id="rainey-street">
+        <h2>Idle Hands on<br>Rainey Street, Austin</h2>
+        <p>21+ Only</p>
+        <p>85 Rainey Street<br>Austin, Texas 78701</p>
+        <p>
+            Monday: 4pm-12am<br>
+            Tuesday: 4pm-12am<br>
+            Wednesday: 4pm-12am<br>
+            Thursday: 2pm-12am<br>
+            Friday: 11am-12am<br>
+            Saturday: 11am-12am<br>
+            Sunday: 11am-12am
+        </p>
+        </div>
+    
+        <div id="lustre-pearl-houston">
+        <h2>Idle Hands at<br>Lustre Pearl Houston, Eado</h2>
+        <p>21+ after 7pm</p>
+        <p>2106 Dallas St, Suite 100<br>Houston, TX 77003</p>
+        <p>
+            Monday: CLOSED<br>
+            Tuesday: CLOSED<br>
+            Wednesday: 2pm-12am<br>
+            Thursday: 2pm-12am<br>
+            Friday: 2pm-12am<br>
+            Saturday: 2pm-12am<br>
+            Sunday: 2pm-12am
+        </p>
+        </div>
+    
+        <div id="parlor-yard">
+        <h2>Parlor & Yard on<br>West 6th, Austin</h2>
+        <p>21+ Only</p>
+        <p>601 West 6th Street<br>Austin, TX 78701</p>
+        <p>
+            Monday: 4pm-12am<br>
+            Tuesday: 4pm-12am<br>
+            Wednesday: 4pm-12am<br>
+            Thursday: 12pm-12am<br>
+            Friday: 12pm-12am<br>
+            Saturday: 12pm-12am<br>
+            Sunday: 12pm-12am
+        </p>
+        </div>
+    
+        <div id="lustre-pearl-south">
+        <h2>Lustre Pearl South<br> in South Austin</h2>
+        <p>Food Truck, All Ages Until 7pm</p>
+        <p>10400 Menchaca Road<br>Austin, Texas</p>
+        <p>
+            Monday: CLOSED<br>
+            Tuesday: 4:00pm-10pm<br>
+            Wednesday: 4:00pm-10pm<br>
+            Thursday: 4:00pm-10pm<br>
+            Friday: 12:00pm-10pm<br>
+            Saturday: 12:00pm-10pm<br>
+            Sunday: 12:00pm-10pm
+        </p>
+        </div>
+    
+        <div id="larimer-square">
+        <h2>Larimer Square, Denver</h2>
+        <div id="denver-p">
+        <p>All ages</p>
+        <p>1441 Larimer St.<br>Denver, CO</p>
+        <p>
+            Monday: 11:30am-12:00am<br>
+            Tuesday: 11:30am-12:00am<br>
+            Wednesday: 11:30am-12:00am<br>
+            Thursday: 11:30am-12:00am<br>
+            Friday: 11:30am-12:00am<br>
+            Saturday: 11:30am-12:00am<br>
+            Sunday: 11:30am-12:00am
+        </p>
+        </div>
+        </div>
+    
+        <div id="east-6th">
+        <h2>East 6th Flagship, Austin</h2>
+        <p>All Ages welcome!</p>
+        <p>1007 East 6th Street<br>Austin, Texas 78702</p>
+        <p>
+            Monday: 11:30am-12am<br>
+            Tuesday: 11:30am-12am<br>
+            Wednesday: 11:30am-12am<br>
+            Thursday: 11:30am-12am<br>
+            Friday: 11:30am-3am<br>
+            Saturday: 11:30am-3am<br>
+            Sunday: 11:30am-12am
+        </p>
+        </div>
+    </div>
+  </div>
+    `
+}
