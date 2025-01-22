17c6ae1f2ada384491c4661823b85c80aa6318ed
Muhammad Mustafa <75218580+mmustafa93@users.noreply.github.com>
2025-01-22 21:15:28 +0500

    Add Homepage

---
+++ src/home.js
@@ -0,0 +1,16 @@
+import "./styles.css";
+import founders from "./assets/founders.png";
+
+export default function Home() {
+  return `
+    <div id="home">
+        <div id="home-text">
+            <h1>NADC Burger</h1>
+            <p>Not a Damn Chance Burger is a wagyu cheeseburger collaboration between Professional Skateboarder Neen Williams and Michelin Starred Chef Phillip Frankland Lee of Sushi by Scratch Restaurants & Pasta Bar.</p>
+        </div>
+        <div id="home-img">
+            <img src=${founders} alt="An Image of Founders" id="founders-image">
+        </div>
+    </div>
+  `;
+}
--- src/index.js
+++ src/index.js
@@ -1,1 +1,12 @@
 import "./styles.css";
+import Home from "./home.js";
+
+const homeBtn = document.querySelector("#home-btn");
+const contentContainer = document.querySelector("#content");
+
+contentContainer.innerHTML = Home();
+
+homeBtn.addEventListener("click", () => {
+    contentContainer.textContent = "";
+    contentContainer.innerHTML = Home();
+});
