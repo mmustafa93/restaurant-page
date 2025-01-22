ea216c8ba30898f152db994684b07c82e7500217
Muhammad Mustafa <75218580+mmustafa93@users.noreply.github.com>
2025-01-23 00:09:27 +0500

    Update Menu page

--- src/menu.js
+++ src/menu.js
@@ -1,6 +1,8 @@
+import burger from './assets/burger.png';
+
 export default function Menu() {
     return `
-        <div id="menu">
+        <div id="menu-page">
             <div id="menu-text">
                 <h1>Menu</h1>
                 <p>Our menu is a collaboration between Professional Skateboarder Neen Williams and Michelin Starred Chef Phillip Frankland Lee of Sushi by Scratch Restaurants & Pasta Bar.</p>
@@ -8,11 +10,14 @@ export default function Menu() {
                     <h3>NADC Burger</h3>
                     <p>Double patty of 100% full-blooded RC Ranch wagyu beef, American cheese, secret sauce, onions, pickles, slightly tamed jalapeños</p>
                 </div>
-                <div id="sides>
+                <div id="sides">
                     <h3>Sides</h3>
                     <p>Fries: Hand-cut fries, sweet potato fries, onion rings</p>
                     <p>Beast Mode Fries: fries with added cheese, diced pickles & slightly tamed jalapeños, special sauce & seasoning</p>
                 </div>
+            </div>
+            <div id="menu-img">
+                <img src=${burger} alt="An Image of a Burger" id="burger-image">
             </div>
         </div>
     `
